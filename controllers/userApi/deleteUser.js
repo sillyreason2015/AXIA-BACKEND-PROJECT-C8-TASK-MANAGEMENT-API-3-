ea6c5e9bd71f385fc744eb2b720b90179240c42b
sillyreason2015 <njatau@outlook.com>
2025-08-15 14:50:40 +0100

    Updated the delete user logic

--- controllers/userApi/deleteUser.js
+++ controllers/userApi/deleteUser.js
@@ -1,5 +1,20 @@
 import User from "../../schema/userSchema.js";
 
 export const deleteUser = async (req, res) => {
-    const {id} = r
+    const {id} = req.params
+    const {_id, isAdmin} = req.user
+    try{
+    const user = await User.findById(id)
+    if(!user){
+        return res.status(400).json({message: "This user does not exist"})
+    }
+
+    if(id.toString() !== _id.toString() && !isAdmin){
+        return res.status(403).json({message: "You are not authorized to carry out this action"})
+    }
+
+    await User.findByIdAndUpdate(id)
+}catch(error){
+    res.status(500).json({message: error.message})
+}
 }
